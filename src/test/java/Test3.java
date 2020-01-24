4f94bf2cfd09f657c25feec5b642a01d1377133d
vkonanikhin <vkonanikhin@cinimex.ru>
2020-01-24 16:36:16 +0300

    test3

--- src/test/java/Test3.java
+++ src/test/java/Test3.java
@@ -1,6 +1,8 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 import java.util.*;
@@ -10,22 +12,43 @@ public class Test3 {
     @Test
     public void test3() {
 
-        ChromeDriver driver = new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        ChromeDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.get("http://localhost/litecart/admin/?app=countries&doc=countries");
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.cssSelector("[type=submit]")).click();
 
-        List<WebElement> country = driver.findElements(By.cssSelector("tr.row a"));
-        List<WebElement> country2 = new LinkedList<WebElement>(country);
-        System.out.println(country2);
+        List<WebElement> count = driver.findElements(By.cssSelector("tr.row a"));
 
-        int i;
-        for (i = 0; i < country.size(); i++) {
-          //  System.out.println(country.get(i).getText());
 
+        ArrayList <String> country1 = new ArrayList<String>();
+        for (int i = 0; i < count.size(); i++) {
+            country1.add(count.get(i).getText());
         }
 
+        ArrayList <String> country2 = country1;
+
+        Collections.sort(country1);
+        for(int i=0; i<country1.size(); i++) {
+          //  System.out.println(country1.get(i));
+        }
+
+        int a = 0;
+        for(String str: country1) { //сортированный список
+            if (!str.equals(country2.get(a))) {
+                System.out.println(String.format("fail %s %s", str, country2.get(a)));
+                return;
+            }
+            a++;
+        }
+
+        System.out.println("ok");
+
+
     }
+
 }
