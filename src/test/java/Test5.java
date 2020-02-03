1c4dc1a16cff3dcb0e9036a2602c6774b6a36493
vkonanikhin <vkonanikhin@cinimex.ru>
2020-02-03 17:35:12 +0300

    Create Test5.java

---
+++ src/test/java/Test5.java
@@ -0,0 +1,36 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class Test5 {
+    @Test
+    public void test5() {
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        ChromeDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.get("http://localhost/litecart");
+
+
+        driver.findElement(By.linkText("New customers click here")).click();
+        driver.findElement(By.name("firstname")).sendKeys("firstName");
+        driver.findElement(By.name("lastname")).sendKeys("lastName");
+        driver.findElement(By.name("address1")).sendKeys("bakuninskiy");
+        driver.findElement(By.name("postcode")).sendKeys("394068");
+        driver.findElement(By.name("city")).sendKeys("Voronezh");
+        driver.findElement(By.name("email")).sendKeys("test111@test111.com");
+        driver.findElement(By.name("phone")).sendKeys("+79103447953");
+        driver.findElement(By.name("password")).sendKeys("test1");
+        driver.findElement(By.name("confirmed_password")).sendKeys("test1");
+
+    }
+}
